2b5a4f97f4a50b0d62c650741ca12d6ce3bab3a7
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-12-04 22:06:47 -0800

    Encode categorical data

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -14,3 +14,15 @@ dataset$Salary = ifelse(
   ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
   dataset$Salary
 )
+
+# Encoding categorical data
+dataset$Country = factor(
+  dataset$Country,
+  levels=c('France', 'Spain', 'Germany'),
+  labels=c(1, 2, 3)
+)
+dataset$Purchased = factor(
+  dataset$Purchased,
+  levels=c('No', 'Yes'),
+  labels=c(0, 1)
+)
