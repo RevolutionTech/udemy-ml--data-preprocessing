e199b6e59be4057d16a978756e26fa428efa18d9
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-12-05 00:29:47 -0800

    Prepare data preprocessing templates for Python and R

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -2,30 +2,7 @@
 
 # Importing the dataset
 dataset = read.csv('Data.csv')
-
-# Taking care of missing data
-dataset$Age = ifelse(
-  is.na(dataset$Age),
-  ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-  dataset$Age
-)
-dataset$Salary = ifelse(
-  is.na(dataset$Salary),
-  ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-  dataset$Salary
-)
-
-# Encoding categorical data
-dataset$Country = factor(
-  dataset$Country,
-  levels=c('France', 'Spain', 'Germany'),
-  labels=c(1, 2, 3)
-)
-dataset$Purchased = factor(
-  dataset$Purchased,
-  levels=c('No', 'Yes'),
-  labels=c(0, 1)
-)
+# dataset = dataset[, 2:3]
 
 # Splitting the dataset into the training and test datasets
 # install.packages('caTools')
@@ -36,5 +13,5 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_set[, 2:3] = scale(training_set[, 2:3])
-test_set[, 2:3] = scale(test_set[, 2:3])
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
